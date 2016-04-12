78463287a204125e1b8cbdf8ea7258df5fe68ae3
Patrick Altman <paltman@gmail.com>
2016-04-12 17:29:37 -0500

    Add back in getCookie

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -12,9 +12,26 @@ const postUrl = url => {
     return axios.post(url);
 };
 
+const getCookie = name => {
+    let cookieValue = null;
+    if (document.cookie && document.cookie !== '') {
+        const cookies = document.cookie.split(';');
+        for (let i = 0; i < cookies.length; i++) {
+            const cookie = jQuery.trim(cookies[i]);
+            // Does this cookie string begin with the name we want?
+            if (cookie.substring(0, name.length + 1) === (name + '=')) {
+                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                break;
+            }
+        }
+    }
+    return cookieValue;
+};
+
 const helpers = {
     getImages,
     postUrl,
+    getCookie
 };
 
 module.exports = helpers;
